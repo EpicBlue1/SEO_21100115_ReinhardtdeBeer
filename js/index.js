0a3f21ae7046774583f2b6b28f9113270dea4e7e
EpicBlue1 <21100115@virtualwindow.co.za>
2022-02-26 23:51:25 +0200

    Tailwind navbar changing between mobile and desktop
    Added android dropdown navigation, automatically scales.
    
    Added logo. And placeholders for navigation items.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 console.log("Workiing");
+
+//grab everything
+const btn = document.querySelector('button.mobile-menu-button');
+const menu = document.querySelector('.mobile-menu');
+
+//add event listings
+btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
