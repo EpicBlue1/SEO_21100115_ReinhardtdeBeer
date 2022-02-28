6d0fc3c0e87ea840fae3ee3afb7c0debe5202f96
EpicBlue1 <21100115@virtualwindow.co.za>
2022-02-28 11:57:55 +0200

    Landing page 70% done
    Added Api into console, basic responsiveness. Changed to crypto instead of tennis. Added second page. 3D model code is added not used yet.

--- js/index.js
+++ js/index.js
@@ -8,3 +8,25 @@ const menu = document.querySelector('.mobile-menu');
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
+
+//all coins
+// const coins = "https://api.coingecko.com/api/v3/coins";
+//     $.getJSON(coins, function(result){
+//         console.log("Coins");
+//         console.log(result);
+//     });
+
+
+//search
+// const coinSearch = "https://api.coingecko.com/api/v3/search?query=bitcoin";
+//     $.getJSON(coinSearch, function(result){
+//         console.log("Coins");
+//         console.log(result);
+//     });
+
+//7 top trending last 24 hours
+// const coinTrending = "https://api.coingecko.com/api/v3/search/trending";
+//     $.getJSON(coinTrending, function(result){
+//         console.log("Local Time");
+//         console.log(result);
+//     });
