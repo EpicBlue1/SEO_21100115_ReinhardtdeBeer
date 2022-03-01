5cba89197676b4dfe899929ad0d421f455dc31fe
EpicBlue1 <21100115@virtualwindow.co.za>
2022-03-01 15:31:51 +0200

    Search works on compare page
    Displays results based on search. Cant open up results yet, due to a strange error. Splitted up dash and compare page js files to try and solve error. Mobile variant links work now to other pages.

---
+++ js/dash.js
@@ -0,0 +1,35 @@
+console.log("Working");
+
+// 7 top trending last 24 hours
+const coinTrending = "https://api.coingecko.com/api/v3/search/trending";
+
+    $.getJSON(coinTrending, function(result){
+        console.log(result.coins);
+
+        for(var i = 0; i < result.coins.length; i++){
+
+            //variable for html
+            var TopCur = `
+            <div class="p-2 h-full bg-gray-900 py-2 px-2 grid md:grid-cols-2 grid-cols-4 text-xs justify-items-center border-2 border-y-white">
+					<div class="text-center text-white text-base">`+result.coins[i].item.name+`</div>
+                    <img src="`+result.coins[i].item.small+`" class="w-7 h-7 bg-white rounded-full"></img>
+					<div class="py-2 text-white">Market Cap:</div>
+					<div class="py-2 text-white">`+result.coins[i].item.market_cap_rank+`</div>
+					<div class="py-2 text-white">Bitcoin Price</div>
+					<div class="py-2 text-white">`+result.coins[i].item.price_btc+`</div>
+			</div>
+            `;
+            //add html to dom
+            $(".TopCurrencies").append(TopCur);
+        
+        };
+    });
+
+    //grab everything
+const btn = document.querySelector('button.mobile-menu-button');
+const menu = document.querySelector('.mobile-menu');
+
+//add event listings
+btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-console.log("Working");
-
 //grab everything
 const btn = document.querySelector('button.mobile-menu-button');
 const menu = document.querySelector('.mobile-menu');
@@ -16,40 +14,58 @@ btn.addEventListener("click", () => {
 //         console.log(result);
 //     });
 
-
-//search
-// const coinSearch = "https://api.coingecko.com/api/v3/search?query=bitcoin";
-//     $.getJSON(coinSearch, function(result){
-//         console.log("Coins");
-//         console.log(result);
-//     });
-
-//7 top trending last 24 hours
-// const coinTrending = "https://api.coingecko.com/api/v3/search/trending";
-//     $.getJSON(coinTrending, function(result){
-//         console.log("Local Time");
-//         console.log(result);
-//     });
-
 // Card Compare page
 // add and remove cards
 // grab everything
-const clickDiv = document.querySelector('div.Selector');
+
+const clickDiv = document.querySelector('Selector');
 const changeInfo = document.querySelector('.Option');
 const changeInfoTwo = document.querySelector('.DisplayOption');
 const removeButton = document.querySelector('.DisplayOption');
 const search = document.querySelector('.Search');
+const topText = document.querySelector('.HeadingCrypto');
 
+search.addEventListener("click", () => {
+    const searchQuery = document. querySelector('input'). value;
+    console.log(searchQuery);
+    
+    // search result
+    const coinSearch = "https://api.coingecko.com/api/v3/search?query=" + searchQuery;
+    $.getJSON(coinSearch, function(result){
+        console.log(result.coins);
+
+        for(var i = 0; i < result.coins.length; i++){
+
+            //variable for html
+            var searchRes = `
+            <Button class="w-full h-16 p-1 bg-gray-800 rounded Option Selector grid grid-cols-3 text-left mb-2">
+                <img src="`+result.coins[i].large+`" class="h-14 w-14 rounded-full bg-white">
+                    <div>`+result.coins[i].name+`</div>
+                <div>
+                    <div>Market Cap Rank</div>
+                    <div>`+result.coins[i].market_cap_rank+`</div>
+                </div>
+            </Button>
+            `;
+            //add html to dom
+            $(".SearchRes").append(searchRes);
+        
+        };
+    });
+});
 
 //add event listings
 clickDiv.addEventListener("click", () => {
     changeInfo.classList.toggle("hidden");
     changeInfoTwo.classList.toggle("hidden");
     search.classList.toggle("hidden");
+    topText.textContent = "Selected Currency";
 });
 
 removeButton.addEventListener("click", () => {
     changeInfo.classList.toggle("hidden");
     changeInfoTwo.classList.toggle("hidden");
     search.classList.toggle("hidden");
+    topText.textContent = "Select an Option";
 });
+
