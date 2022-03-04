450e8c0ecb2a65c196f64a93ef460ae081d02ef7
EpicBlue1 <21100115@virtualwindow.co.za>
2022-03-04 23:35:26 +0200

    Search and clicking on result almost works
    When searching for something a second time, clicking on a result breaks the "More information" on selected result. It disappears. Working on a fix rather using toggle than add or remove class.

--- js/index.js
+++ js/index.js
@@ -14,6 +14,8 @@ btn.addEventListener("click", () => {
 //         console.log(result);
 //     });
 
+//first search block
+
 // Card Compare page
 // add and remove cards
 // grab everything
@@ -48,22 +50,87 @@ search.addEventListener("click", () => {
 
 
             //open result   
-			$("Button").click(function(){
+			$(".Option").click(function(){
 			    $(".Selector").addClass("hidden");
-                $(".DisplayOption").removeClass("hidden");
+                $(this).closest(".DisplayOption").toggle("hidden");
+                $(".Search").addClass("hidden");
+                console.log("CLicked on a result")
             })
 
             //close result
             $(".close").click(function(){
 			    $(".Selector").removeClass("hidden");
-                $(".DisplayOption").addClass("hidden");
+                $(this).closest(".DisplayOption").toggle("hidden");
+                $(".Search").removeClass("hidden");
+                console.log("Closed result")
             })
 
             //clear results when new search
             $(".SearchOne").click(function(){
 			    $(".SearchRes").empty();
-            })
+                $(".DisplayOption").empty();
+            });
 
 		};
 	});
 });
+
+//first search block
+
+// Card Compare page
+// add and remove cards
+// grab everything
+// const searchtwo = document.querySelector('.SearchOne');
+
+// searchtwo.addEventListener("click", () => {
+//     //get value from input
+// 	const searchQuery = document.querySelector('input').value;
+
+// 	// search result
+//     //insert value from input into url
+// 	const coinSearch = "https://api.coingecko.com/api/v3/search?query=" + searchQuery;
+
+// 	$.getJSON(coinSearch, function(result) {
+
+// //loop through results and add to html
+// 		for (var i = 0; i < result.coins.length; i++) {
+
+// 			//variable for html
+// 			var searchRes = `
+//             <Button class="w-full h-16 p-1 bg-gray-800 rounded Option Selector grid grid-cols-3 text-left mb-2">
+//                 <img src="` + result.coins[i].large + `" class="h-14 w-14 rounded-full bg-white">
+//                     <div>` + result.coins[i].name + `</div>
+//                 <div>
+//                     <div>Market Cap Rank</div>
+//                     <div>` + result.coins[i].market_cap_rank + `</div>
+//                 </div>
+//             </Button>
+//             `;
+// 			//add html to dom
+//             $(".SearchRes").append(searchRes);
+
+
+//             //open result   
+// 			$(".Option").click(function(){
+// 			    $(".Selector").addClass("hidden");
+//                 $(".DisplayOption").removeClass("hidden");
+//                 $(".Search").addClass("hidden");
+//                 console.log("CLicked on a result")
+//             })
+
+//             //close result
+//             $(".close").click(function(){
+// 			    $(".Selector").removeClass("hidden");
+//                 $(".DisplayOption").addClass("hidden");
+//                 $(".Search").removeClass("hidden");
+//                 console.log("Closed result")
+//             })
+
+//             //clear results when new search
+//             $(".SearchOne").click(function(){
+// 			    $(".SearchRes").empty();
+//             })
+
+// 		};
+// 	});
+// });
